27c712fce933e7a83ff999810eab3d46061bd98b
Derrick Bozich <dbozich1@gmail.com>
2020-04-29 22:57:00 -0600

    working on serial input

--- 74HC165-WiringPi-master/ogm-real-code-test/CM3GPIO.cpp
+++ 74HC165-WiringPi-master/ogm-real-code-test/CM3GPIO.cpp
@@ -18,7 +18,6 @@
 #define SR_CLOCK 18             // CLK pin 
 
 
-
 CM3GPIO::CM3GPIO() {
 }
 
@@ -43,6 +42,7 @@ void CM3GPIO::init(){
     // get initial pin states
     shiftRegRead();
     pinValuesLast = pinValues;
+    displayPinValues();
 
 }
 
@@ -57,23 +57,11 @@ void CM3GPIO::poll(){
     // get key values if a key pin changed
     if (pinValues  != pinValuesLast ) {
          displayPinValues();
-         getKeys();
-       
     }
    
-    
     pinValuesLast = pinValues;
 
-   
-    
-    // check encoder, gotta check every time for debounce purposes
-    getEncoder();
-    
 }
-
-
-
-
 
 uint32_t CM3GPIO::shiftRegRead(void)
 {
@@ -133,104 +121,8 @@ uint32_t CM3GPIO::shiftRegRead(void)
     return(bytesVal);
 }
 
-void CM3GPIO::getKeys(void){
-    keyStates = 0;
-    
-    keyStates |= (pinValues >> (0 + 7) & 1) << 24;
-    keyStates |= (pinValues >> (1 + 7) & 1) << 16;
-    keyStates |= (pinValues >> (2 + 7) & 1) << 17;
-    keyStates |= (pinValues >> (3 + 7) & 1) << 18;
-    keyStates |= (pinValues >> (4 + 7) & 1) << 19;
-    keyStates |= (pinValues >> (5 + 7) & 1) << 20;
-    keyStates |= (pinValues >> (6 + 7) & 1) << 21;
-    keyStates |= (pinValues >> (7 + 7) & 1) << 22;
-   
-    keyStates |= (pinValues >> (8 + 7) & 1) << 23;
-    keyStates |= (pinValues >> (9 + 7) & 1) << 8;
-    keyStates |= (pinValues >> (10 + 7) & 1) << 9;
-    keyStates |= (pinValues >> (11 + 7) & 1) << 10;
-    keyStates |= (pinValues >> (12 + 7) & 1) << 11;
-    keyStates |= (pinValues >> (13 + 7) & 1) << 12;
-    keyStates |= (pinValues >> (14 + 7) & 1) << 13;
-    keyStates |= (pinValues >> (15 + 7) & 1) << 14;
-    
-    keyStates |= (pinValues >> (16 + 7) & 1) << 15;
-    keyStates |= (pinValues >> (17 + 7) & 1) << 0;
-    keyStates |= (pinValues >> (18 + 7) & 1) << 1;
-    keyStates |= (pinValues >> (19 + 7) & 1) << 2;
-    keyStates |= (pinValues >> (20 + 7) & 1) << 3;
-    keyStates |= (pinValues >> (21 + 7) & 1) << 4;
-    keyStates |= (pinValues >> (22 + 7) & 1) << 5;
-    keyStates |= (pinValues >> (23 + 7) & 1) << 6;
-    
-    keyStates |= (pinValues >> (24 + 7) & 1) << 7;
-    
-    keyStates |= (0xFE000000);  // zero out the bits not key bits
-    keyStates = ~keyStates;
-}
 
-void CM3GPIO::getEncoder(void){
 
-	static uint8_t encoder_last = 0;
-	uint8_t encoder = 0;
-
-	#define PRESS 0
-	#define RELEASE 1
-	uint8_t button;
-	static uint8_t button_last = RELEASE;
-	static uint8_t press_count = 0;
-	static uint8_t release_count = 0;
-
-	button = (pinValues >> 4) & 0x1;
-	if (button == PRESS) {
-		press_count++;
-		release_count = 0;
-	}
-	if ((press_count > 10) && (button_last == RELEASE)){	// press
-			button_last = PRESS;
-			release_count = 0;
-            encBut = 1;
-            encButFlag = 1;
-	}
-
-	if (button == RELEASE) {
-		release_count++;
-		press_count = 0;
-	}
-	if ((release_count > 10) && (button_last == PRESS)){	// release
-			button_last = RELEASE;
-			press_count = 0;
-            encBut = 0;
-            encButFlag = 1;
-	}
-
-	// turning
-	encoder = (pinValues >> 5) & 0x3;
-	
-    if (encoder != encoder_last) {
-        if (encoder_last == 0) {
-	    if (encoder == 2){
-	        encTurn = 1;
-                encTurnFlag = 1;
-            }
-            if (encoder == 1){
-                encTurn = 0;
-                encTurnFlag = 1; 
-	    }
-        }
-        if (encoder_last == 3) {
-	    if (encoder == 1){
-                encTurn = 1;
-                encTurnFlag = 1;
-	    }
-            if (encoder == 2){
-                encTurn = 0;
-                encTurnFlag = 1;
-            }
-        }
-        encoder_last = encoder;
-	}
-}
 
 
 
--- 74HC165-WiringPi-master/ogm-real-code-test/CM3GPIO.h
+++ 74HC165-WiringPi-master/ogm-real-code-test/CM3GPIO.h
@@ -11,6 +11,7 @@
 #include <time.h>
 
 
+
 class CM3GPIO
 {
     public:
